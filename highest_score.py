3af628239af0355bb3241a2a9b4db172efff10df
Pavan K <118459281+Pavan19047@users.noreply.github.com>
2023-12-19 11:08:14 +0530

    Update highest_score.py's logic

--- highest_score.py
+++ highest_score.py
@@ -5,7 +5,12 @@ for i in range(3):
     score = int(input())
     students[name] = score
 
+highest_score = None
+highest_scorer = None
 
-highest_scorer = max(students, key=students.get)
+for student, score in students.items():
+    if highest_score is None or score > highest_score:
+        highest_score = score
+        highest_scorer = student
 
-print({highest_scorer: students[highest_scorer]})
+print({highest_scorer: highest_score})
